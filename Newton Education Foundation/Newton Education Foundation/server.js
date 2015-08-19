d28ac0f607b67eef65786c5acea48a639f0f55e0
James Parsons <jamesmp98@gmail.com>
2015-08-19 19:42:33 -0400

    Finished integrating frontend.

--- Newton Education Foundation/Newton Education Foundation/server.js
+++ Newton Education Foundation/Newton Education Foundation/server.js
@@ -1,19 +1,22 @@
-﻿// Main serve file.
+﻿// Main server file.
 var express = require("express");
+var bodyParser = require("body-parser");
 var api = require("./api/events-api.js");
 
 // Get port from environment variables or set it to 3000.
 var port = process.env.port || 3000;
 
 var app = express();
+// Set up static files.
+app.use(express.static("public"));
+// Body parser.
+app.use(bodyParser.urlencoded());
 // Mount the API.
 app.use(api);
 
 // Route the index page.
 app.get("/", function (req, res) {
-    // res.send("./public/index.html");
-    // Testing the gulpfile.
-    res.send("hi");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 // Donations
