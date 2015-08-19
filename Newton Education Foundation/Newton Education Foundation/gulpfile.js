d67511c41fae42a078d49d1773c065cf3b4394ff
James Parsons <jamesmp98@gmail.com>
2015-08-19 17:51:09 -0400

    Huge restructuring. Still have to work out a few kinks and integrate the frontend.

---
+++ Newton Education Foundation/Newton Education Foundation/gulpfile.js
@@ -0,0 +1,12 @@
+﻿var gulp = require("gulp");
+var spawn = require("child_process").spawn;
+
+gulp.task("run", function () {
+    spawn("node", ["server.js"]);
+});
+
+gulp.task("test", function () {
+    spawn("mocha", ["./test/tests.js"]);
+});
+
+// TODO: Write more tasks.
